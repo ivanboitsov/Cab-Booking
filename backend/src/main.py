ad86f2d3ff06b7d83a899f31e33535862611a012
Ivan <ivanboitsov386@gmail.com>
2024-10-15 02:21:28 +0300

    Endpoints for houses

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,18 +1,18 @@
 import logging
 
-from fastapi import FastAPI, APIRouter, Depends
+from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
 
 from src.auth.router import user_router
-from src.db.database import get_db
+from src.house.router import house_router
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-router = APIRouter(prefix="/api/v1/bookingCar")
+router = APIRouter(prefix="/api/taksa")
 
 router.include_router(user_router)
+router.include_router(house_router)
 
 app = FastAPI()
 
